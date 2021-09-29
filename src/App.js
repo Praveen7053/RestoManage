523058cb316a8acd7a33008d24294997f0c5bb74
Praveen Kumar <69811065+Praveen7053@users.noreply.github.com>
2021-09-29 14:42:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Restorent from "./component/Basics/Restorent";
+// import UseState from "./Hooks/useState";
+// import UseEffect from "./Hooks/useEffect";
+// import UseReducer from "./Hooks/useReducer";
+
+const App = () => {
+  return(
+    <>
+      <Restorent /> 
+      {/* <UseState /> */}
+      {/* <UseEffect /> */}
+     {/* <UseReducer/> */}
+    </> 
+  );
+};
+
+export default App
---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,122 @@
+const Menu = [
+    {
+        id:1,
+        image:"images/pexels-alena-shekhovtcova-6941026.jpg",
+        name:"chicken",
+        category:"lunch",
+        price:150,
+        description:"This is our lunch item  Pastes an item from the clipboard ring tab of the Toolbox at the cursor in the file and automatically selects the pasted item.Cycle through the items on the clipboard by pressing the shortcut keys repeatedly.",
+    },
+
+    {
+        id:2,
+        image:"images/pexels-daria-shevtsova-1095550.jpg",
+        name:"Palak Paneer",
+        category:"lunch",
+        price:220,
+        description:"This is our lunch item  Pastes an item from the clipboard ring tab of the Toolbox at the cursor in the file and automatically selects the pasted item.Cycle through the items on the clipboard by pressing the shortcut keys repeatedly.",
+    },
+
+    {
+        id:3,
+        image:"images/pexels-ella-olsson-1640777.jpg",
+        name:"Samose",
+        category:"lunch",
+        price:680,
+        description:"This is our lunch item  Pastes an item from the clipboard ring tab of the Toolbox at the cursor in the file and automatically selects the pasted item.Cycle through the items on the clipboard by pressing the shortcut keys repeatedly.",
+    },
+
+    {
+        id:4,
+        image:"images/pexels-engin-akyurt-1437267.jpg",
+        name:"Panir Tikka",
+        category:"lunch",
+        price:400,
+        description:"This is our lunch item  Pastes an item from the clipboard ring tab of the Toolbox at the cursor in the file and automatically selects the pasted item.Cycle through the items on the clipboard by pressing the shortcut keys repeatedly.",
+    },
+
+    
+    {
+        id:5,
+        image:"images/pexels-harry-dona-2338407.jpg",
+        name:"Tandoori Chicken",
+        category:"Breakfast",
+        price:120,
+        description:"This is our Breakfast item  Pastes an item from the clipboard ring tab of the Toolbox at the cursor in the file and automatically selects the pasted item.Cycle through the items on the clipboard by pressing the shortcut keys repeatedly.",
+    },
+
+    {
+        id:6,
+        image:"images/pexels-mareefe-678414.jpg",
+        name:"Poha recipe",
+        category:"Breakfast",
+        price:100,
+        description:"This is our Breakfast item  Pastes an item from the clipboard ring tab of the Toolbox at the cursor in the file and automatically selects the pasted item.Cycle through the items on the clipboard by pressing the shortcut keys repeatedly.",
+    },
+
+    {
+        id:7,
+        image:"images/pexels-mariana-kurnyk-1775043.jpg",
+        name:"Bread upma",
+        category:"Breakfast",
+        price:110,
+        description:"This is our Breakfast item  Pastes an item from the clipboard ring tab of the Toolbox at the cursor in the file and automatically selects the pasted item.Cycle through the items on the clipboard by pressing the shortcut keys repeatedly.",
+    },
+
+    {
+        id:8,
+        image:"images/pexels-marvin-ozz-2474658.jpg",
+        name:"Samose",
+        category:"Breakfast",
+        price:130,
+        description:"This is our Breakfast item  Pastes an item from the clipboard ring tab of the Toolbox at the cursor in the file and automatically selects the pasted item.Cycle through the items on the clipboard by pressing the shortcut keys repeatedly.",
+    },
+
+    {
+        id:9,
+        image:"images/pexels-photomix-company-103566.jpg",
+        name:"Milk Sheck",
+        category:"Dinner",
+        price:90,
+        description:"This is our Dinner item  Pastes an item from the clipboard ring tab of the Toolbox at the cursor in the file and automatically selects the pasted item.Cycle through the items on the clipboard by pressing the shortcut keys repeatedly.",
+    },
+
+    {
+        id:10,
+        image:"images/pexels-rajesh-tp-1600711.jpg",
+        name:"Butter Chicken",
+        category:"Dinner",
+        price:240,
+        description:"This is our Dinner item  Pastes an item from the clipboard ring tab of the Toolbox at the cursor in the file and automatically selects the pasted item.Cycle through the items on the clipboard by pressing the shortcut keys repeatedly.",
+    },
+
+    {
+        id:11,
+        image:"images/pexels-surabhi-siddaiah-1051399.jpg",
+        name:"Palak Panir",
+        category:"Dinner",
+        price:240,
+        description:"This is our Dinner item  Pastes an item from the clipboard ring tab of the Toolbox at the cursor in the file and automatically selects the pasted item.Cycle through the items on the clipboard by pressing the shortcut keys repeatedly.",
+    },
+
+    {
+        id:12,
+        image:"images/pexels-ella-olsson-3026808.jpg",
+        name:"Meat",
+        category:"Dinner",
+        price:240,
+        description:"This is our Dinner item  Pastes an item from the clipboard ring tab of the Toolbox at the cursor in the file and automatically selects the pasted item.Cycle through the items on the clipboard by pressing the shortcut keys repeatedly.",
+    },
+
+    {
+        id:13,
+        image:"images/pexels-ella-olsson-3026805.jpg",
+        name:"Fastfood",
+        category:"Evening",
+        price:220,
+        description:"This is our Dinner item  Pastes an item from the clipboard ring tab of the Toolbox at the cursor in the file and automatically selects the pasted item.Cycle through the items on the clipboard by pressing the shortcut keys repeatedly.",
+    },
+
+];
+
+export default Menu;
